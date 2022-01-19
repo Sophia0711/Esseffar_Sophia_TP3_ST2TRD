0110df9ed31fb29fc89f18214b9164eb2f0b0ed6
Sophia0711 <71383610+Sophia0711@users.noreply.github.com>
2022-01-19 21:47:26 +0100

    Add files via upload

---
+++ Esseffar_Sophia_TP3/Esseffar_Sophia_TP3/Exercise1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esseffar_Sophia_TP3
+{
+    public static class Exercise1
+    {
+        public static void queries()
+        {
+            var movies = new MovieCollection().Movies;
+
+            //
+
+            var oldmovie = movies.OrderBy(movies => movies.ReleaseDate).First().Title;
+
+            Console.WriteLine(oldmovie);
+
+            //
+
+            var total = movies.Count();
+
+            Console.WriteLine(total);
+
+            //
+
+            var counte = movies.Count(movie => movie.Title.Contains("e"));
+
+            Console.WriteLine(counte);
+
+            //
+
+            int nb = 0;
+            foreach (var movie in movies)
+            {
+                nb += movie.Title.Count(movie => movie == 'f');
+            }
+
+            Console.WriteLine(nb);
+
+            //
+
+            var budget = movies.OrderBy(movies => movies.Budget).Last().Title;
+
+            Console.WriteLine(budget);
+
+            //
+
+            var bo = movies.OrderBy(movies => movies.BoxOffice).First().Title;
+
+            Console.WriteLine(bo);
+
+            //
+
+            var reverse = movies.OrderByDescending(movies => movies.Title).Take(11);
+
+            foreach (var movie in reverse)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+            //
+
+
+            var before = movies.Count(movie => movie.ReleaseDate < new DateTime(1980));
+
+            Console.WriteLine(before);
+
+            //
+
+
+            //
+
+            var letters = movies
+                .Where(movie => (movie.Title.Contains("H")) || (movie.Title.Contains("W")))
+                .Where(movie => !((movie.Title.Contains("I")) || (movie.Title.Contains("T"))));
+
+            foreach (var movie in letters)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+            //
+
+            var averageBudget = movies.Average(movie => movie.Budget);
+            var averageBox = movies.Average(movie => movie.BoxOffice);
+
+            Console.WriteLine(averageBudget);
+            Console.WriteLine(averageBox);
+        }
+    }
+}
---
+++ Esseffar_Sophia_TP3/Esseffar_Sophia_TP3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Esseffar_Sophia_TP3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Exercise1.queries();
+
+            
+            /*
+            
+            Mutex mut = new Mutex();
+
+
+            var t1 = new Thread(f1 => print(' ', 50, 10,mut));
+            var t2 = new Thread(f2 => print('*', 40, 11, mut));
+            var t3 = new Thread(f3 => print('°', 20, 9,mut));
+
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+
+            */
+        }
+
+        public static void print(char v1, int time, int duration, Mutex mut)
+        {
+            int nb = (duration*1000) / time;
+
+            for (int i = 0; i <= nb; i++)
+            {
+                mut.WaitOne();
+                Console.Write(v1);
+                mut.ReleaseMutex();
+                Thread.Sleep(time);
+            }
+        }
+
+    }
+}
